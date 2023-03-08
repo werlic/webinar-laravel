c5165177150cec66fe1e3c067770922dc557469d
werlic <agussulaeman453@yahoo.co.id>
2023-03-09 00:24:31 +0700

    Add migration and frontend set up

---
+++ database/migrations/2023_03_08_135909_create_webinars_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('webinars', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 255)->required();
+            $table->string('description', 255)->required();
+            $table->string('speaker', 255)->required();
+            $table->string('moderator', 255)->required();
+            $table->foreignId('category_id')->nullable()->constrained()->onUpdate('cascade')->onDelete('set NULL');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('webinars');
+    }
+};
